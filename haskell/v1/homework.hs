968004189704d9a8203872b52ff3a3b552de552f
njegg <njeggomir15@gmail.com>
2022-11-01 17:12:24 +0100

    homework 1 done

--- haskell/v1/homework.hs
+++ haskell/v1/homework.hs
@@ -1,4 +1,4 @@
--- 1. Napisati funkciju koja uklanja svaki N-ti element liste.
+-- 1. Napisati funkciju koja uklanja svaki N-ti element liste. --
 rmNth :: [Int] -> Int -> [Int]
 rmNth l n = rmNthHelper l n 0
 
@@ -9,7 +9,7 @@ rmNthHelper (x:xs) n i
   | otherwise = x : rmNthHelper(xs) n (i + 1)
 
 
--- 2. Napisati funkciju koja vraca listu delilaca nekog pozitivnog broja.
+-- 2. Napisati funkciju koja vraca listu delilaca nekog pozitivnog broja. --
 delioci :: Int -> [Int] 
 deliociHelper :: Int -> Int -> [Int] -> [Int]
 
@@ -20,7 +20,8 @@ deliociHelper n i l
   | mod n i == 0 = deliociHelper n (i + 1) (l ++ [i])
   | otherwise = deliociHelper n (i + 1) l
 
--- 3. Napisati quicksort.
+
+-- 3. Napisati quicksort. --
 qs :: [Int] -> [Int]
 qs [] = []
 qs [x] = [x]
@@ -29,20 +30,50 @@ qs (x:xs) = (qs lx) ++ [x] ++ (qs gx)
     lx = filter (< x) xs
     gx = filter (>= x) xs
 
--- 4 Definisati filter'' f l preko ZF izraza
+
+-- 4 Definisati filter'' f l preko ZF izraza --
 filter'' :: (Int -> Bool) -> [Int] -> [Int]
 filter'' f l = [x | x <- l, f x]
 
--- 5. Napisati funkciju koja sumira sve elemente liste.
+
+-- 5. Napisati funkciju koja sumira sve elemente liste. --
 sum' :: [Int] -> Int
 sum' = foldl (+) 0
 
 sum'' :: [Int] -> Int
-sum'' :: [Int] -> Int
+sum'' [] = 0
+sum'' [x] = x
+sum'' (x:xs) = x + sum'' xs
 
--- 6. Napisati funkciju zip' koja prima 2 liste i sabira elemente na istim indeksima. 
--- 7. Napisati funkciju koja sumira sve cifre prosledjenog broja:
+
+-- 6. Napisati funkciju zip' koja prima 2 liste i sabira elemente na istim indeksima. --
+zip' :: [Int] -> [Int] -> [Int]
+zip' [] [] = [] 
+zip' [] [x] = [x] 
+zip' [x] [] = [x] 
+zip' (x:xs) (y:ys) = (x + y) : zip' xs ys
+
+
+-- 7. Napisati funkciju koja sumira sve cifre prosledjenog broja: --
 -- 	a. rekurzivno
+digisumR :: Int -> Int
+digisumR 0 = 0
+digisumR n = mod n 10 + digisumR (div n 10)
+
+
 -- 	b. repno rekurzivno
--- 8. Sumirati parne cifre nekog broja 
--- 
+digisumTR :: Int -> Int
+digisumTR 0 = 0
+digisumTR n = digisumAcum n 0
+
+digisumAcum :: Int -> Int -> Int
+digisumAcum 0 a = a
+digisumAcum n a = digisumAcum (div n 10) (a + mod n 10)
+
+
+-- 8. Sumirati parne cifre nekog broja --
+evenDigisum :: Int -> Int
+evenDigisum 0 = 0
+evenDigisum n
+  | even (mod n 10) = mod n 10 + evenDigisum (div n 10) 
+  | otherwise = evenDigisum (div n 10)
